16b73507776c8d710ed60ed54a8683ea75663d8b
Aliaksei <e79eas@gmail.com>
2013-10-12 23:39:09 +0300

    env, osx

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -3,3 +3,6 @@ defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool 
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
---
+++ ruby/env.zsh
@@ -0,0 +1,4 @@
+export GEM_HOME=$HOME/ruby/gems
+export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
+export GEM_CACHE=$GEM_HOME/cache
+export PATH=$PATH:$HOME/ruby/gems/bin
