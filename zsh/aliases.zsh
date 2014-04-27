9be521293fba6ffb184f328fb91338b601fa4151
Aliaksei Shytkin <e79eas@gmail.com>
2014-04-27 18:59:45 +0300

    Some new cool functions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,27 @@ alias aliases_remote_update='wget -q -O - "$@" https://alias.sh/user/8953/alias 
 alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 
 alias sourcetree='open -a SourceTree'
+alias chrome="open -a google\ chrome"
+alias finder='open -a Finder'
+alias gh="open 'http://github.com/roadhump'"
+
+function findfile() {
+    find . -iname "*$1*" -ls
+}
+
+# Create a data URL from a file
+function dataurl() {
+        local mimeType=$(file -b --mime-type "$1")
+        if [[ $mimeType == text/* ]]; then
+                mimeType="${mimeType};charset=utf-8"
+        fi
+        echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')"
+}
+
+function subln() {
+        if [ $# -eq 0 ]; then
+                subl -n .
+        else
+                subl -n "$@"
+        fi
+}
