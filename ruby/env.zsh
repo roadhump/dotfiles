6266a47384ff304de81d00a12fc3a34efbfc5676
Aliaksei Shytkin <e79eas@gmail.com>
2015-02-16 15:52:24 +0300

    Some unsorted fixes

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -3,7 +3,6 @@ export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 
 export GEM_HOME=$HOME/.ruby/gems
-export GEM_PATH=$GEM_HOME:/usr/lib/ruby/gems/1.8
+export GEM_PATH=$GEM_HOME
 export GEM_CACHE=$GEM_HOME/cache
-
 export PATH=$PATH:$GEM_HOME/bin
