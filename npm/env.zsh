0aeaa9e77df07cd5065b54c4a70e981444b3f37f
Aliaksei Shytkin <e79eas@gmail.com>
2015-05-07 09:46:14 +0300

    Fix temporary paths

--- npm/env.zsh
+++
@@ -1,8 +0,0 @@
-# NPM packages in homedir
-export NPM_PACKAGES="$HOME/.npm-packages"
-export PATH=$PATH:$NPM_PACKAGES/bin
-
-unset MANPATH  # delete if you already modified MANPATH elsewhere in your config
-export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
-
-export NODE_PATH="$NPM_PACKAGES/lib/node_modules:$NODE_PATH"
